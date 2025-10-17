1032d6acaabe433f87ff3dc68b7cd707b4f0fdca
Victor Jegede <victor.jegede.90@gmail.com>
2025-10-17 14:52:14 +0100

    Hide Checkout

--- shakara-festival/src/app/checkout/page.tsx
+++ shakara-festival/src/app/checkout/page.tsx
@@ -1,8 +1,14 @@
 import V2Layout from '@/components/v2/Layout'
 import ThemedContent from '@/components/ThemedContent'
 import CheckoutForm from '@/components/v2/checkout/CheckoutForm'
+import { CHECKOUT_ENABLED } from '@/lib/featureFlags'
+import { notFound } from 'next/navigation'
 
 export default function CheckoutPage() {
+  if (!CHECKOUT_ENABLED) {
+    // Hide page entirely when flag is off
+    notFound()
+  }
   return (
     <V2Layout currentPageName="Checkout">
       <ThemedContent transparent>
